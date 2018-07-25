d0af853e908e34d5e3524e4c4190fbe493400c0e
Xaviera <32984629+xvasser@users.noreply.github.com>
2018-07-25 18:53:33 -0500

    updated footer
    added social media

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
 
 <br>
 <footer>
-    <img src="chef.png" alt="Chef" height="127.63px" width="73.41px"> <p>Copyright 2018</p>
+    <img src="chef.png" alt="Chef" height="127.63px" width="73.41px"> 
+    <div>
+        <a href="[full link to your Twitter]">
+        <img title="Twitter" alt="Twitter" src="https://socialmediawidgets.files.wordpress.com/2014/03/01_twitter.png" width="35" height="35" />
+        </a>
+        <a href="[full link to your Facebook page]">
+        <img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
+        </a>
+     <p>Copyright 2018</p>
 </footer>
 
     </body>
