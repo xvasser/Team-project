f9befdc82574939c1b3e93c4397a20484b39e881
Xaviera <32984629+xvasser@users.noreply.github.com>
2018-07-25 18:59:44 -0500

    Update footer.php
    forgot to close the div

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
         <a href="[full link to your Facebook page]">
         <img title="Facebook" alt="Facebook" src="https://socialmediawidgets.files.wordpress.com/2014/03/02_facebook.png" width="35" height="35" />
         </a>
+     </div>   
      <p>Copyright 2018</p>
 </footer>
 
